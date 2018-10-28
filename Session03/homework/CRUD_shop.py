997c642e9290360ea4340365ccaa60d34ae6057d
Tam Bui <tambv6164@gmail.com>
2018-10-28 12:28:52 +0700

    initial commit

--- Session03/homework/CRUD_shop.py
+++ Session03/homework/CRUD_shop.py
@@ -10,25 +10,24 @@ while (act in act_list):
     if act == "Stop":
         print("Thanks for coming. See you next time!")
         break
-    else: 
-        if act == "C":
+    elif act == "C":
+        new = input("Enter new item: ")
+        items.append(new)
+    elif act == "U" or act == "D":
+        if act =="U":
+            pos = int(input("Update position? "))
+        else:
+            pos = int(input("Delete position? "))
+        while pos < 1 or pos > len(items):
+            print("Position must start from 1 and we only have", len(items), "items now")
+            pos = int(input("Position again? "))
+        if act == "U":
             new = input("Enter new item: ")
-            items.append(new)
-        elif act == "U" or act == "D":
-            if act =="U":
-                pos = int(input("Update position? "))
-            else:
-                pos = int(input("Delete position? "))
-            while pos < 1 or pos > len(items):
-                print("Position must start from 1 and we only have", len(items), "items now")
-                pos = int(input("Position again? "))
-            if act == "U":
-                new = input("Enter new item: ")
-                items[pos - 1] = new
-            else: 
-                items.pop(pos - 1)
-        print("Our items: ", end = "")
-        print(*items, sep = ", ")
-        act = input("Done. What do you want to do next: C, R, U, D or Stop? ")
-        while (act not in act_list):
-            act = input("Please type exactly the action (notice upper case): C, R, U, D or Stop? ")
+            items[pos - 1] = new
+        else: 
+            items.pop(pos - 1)
+    print("Our items: ", end = "")
+    print(*items, sep = ", ")
+    act = input("Done. What do you want to do next: C, R, U, D or Stop? ")
+    while (act not in act_list):
+        act = input("Please type exactly the action (notice upper case): C, R, U, D or Stop? ")
