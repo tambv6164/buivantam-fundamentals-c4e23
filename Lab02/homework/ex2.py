8ac68735b4483f940f4b7e2f59969bc4a95511b1
Tam Bui <tambv6164@gmail.com>
2018-11-08 08:21:01 +0700

    initial commit

--- Lab02/homework/ex2.py
+++ Lab02/homework/ex2.py
@@ -1,7 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from collections import OrderedDict
-from random import choice
 import pyexcel
 
 url ="http://s.cafef.vn/bao-cao-tai-chinh/VNM/IncSta/2017/3/0/0/ket-qua-hoat-dong-kinh-doanh-cong-ty-co-phan-sua-viet-nam.chn"
@@ -14,15 +13,9 @@ soup = BeautifulSoup(page_content, "html.parser")
 header = soup.find("table", id="tblGridData").tr.find_all("td")
 table = soup.find("table", id="tableContent").find_all("tr")
 
-firstrow = ["A", "B", "C", "D", "E"]
-
 tableheader = []
 for i in header:
-    a = i.string
-    if a is None:
-        a = choice(firstrow)
-        firstrow.remove(a)
-    a = a.strip()
+    a = i.text.strip()
     tableheader.append(a)
 tableheader.pop()
 
@@ -31,14 +24,10 @@ for td in table:
     a = td.find_all("td")
     a_list = []
     for i in a:
-        b = i.string
+        b = i.text.strip()
         a_list.append(b)
-    if a_list != [] and (not all(v is None for v in a_list)):
+    if a_list != [] and (not all(v == "" for v in a_list)):
         a_list = a_list[:5]
-        for i in range(len(a_list)):
-            if a_list[i] is None:
-                a_list[i] = ""
-            a_list[i] = a_list[i].strip()
         tablecontent.append(a_list)
 
 tablefull = []
